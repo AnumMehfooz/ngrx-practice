d4f6adfb48461dbee818d0337a7f6f22b8375411
Anum Mahfooz <anum_mahfooz@hotmail.com>
2023-03-14 16:48:19 +0500

    code refactored

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,20 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit{
+export class CounterOutputComponent implements OnInit,OnDestroy{
   // @Input () counter: any;
   counter:number | undefined;
-  constructor(private store:Store<{counter:{counter:number}}>){}
+  counterSubscription: Subscription | undefined;
+
+  constructor(private store:Store<{counter:CounterState}>){}
+  
   ngOnInit(): void {
     this.store.select('counter').subscribe((data) =>{
       this.counter = data.counter;
     })
     // throw new Error('Method not implemented.');
+  }
+
+  ngOnDestroy(): void {
+    if(this.counterSubscription){
+      this.counterSubscription.unsubscribe();
+    }
   }
 
 }
--- src/app/counter/state/counter.state.ts
+++ src/app/counter/state/counter.state.ts
@@ -1,4 +1,7 @@
 //state for counter
-export const initialState = {
+export interface CounterState{
+    counter: number;
+}
+export const initialState:CounterState = {
     counter: 0
 }
